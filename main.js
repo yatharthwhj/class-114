a1f3e4f3e3c46fc26a8856c11cb68a5e6304b567
yatharthwhj <103936260+yatharthwhj@users.noreply.github.com>
2023-12-21 17:47:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+canvas = createCanvas(300,300);
+canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+save('Yatz.Png');
+}
